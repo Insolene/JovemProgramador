8801b50a3a3ec8fc3e736c8006a7510908c6c6c6
Gabriel De Souza <gabrielsesilva47@gmail.com>
2024-09-12 10:00:44 -0300

    Inserido uma controller de idade

---
+++ JovemProgramador/Controllers/IdadeController.cs
@@ -0,0 +1,19 @@
+﻿using Empresa.Endereco.Servico;
+using Empresa.Endereco.Servico.ViaCep;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Empresa.Endereco.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IdadeController:ControllerBase
+    {   
+        [HttpGet(), Route("ObterIdade")]    
+        public IActionResult ObterIdade(DateTime dataNascimento)
+        {
+            int idade = new IdadeService().BuscarIdade(dataNascimento);
+            return Ok(idade); 
+        }
+
+    }
+}
